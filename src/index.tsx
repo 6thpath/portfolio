862ea57411118632e5758a7456d010557d23babd
Phát <tienphat1501@gmail.com>
2022-03-06 20:59:23 +0700

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { render } from 'react-dom'
+
+import './index.css'
+
+import App from 'App'
+import reportWebVitals from 'reportWebVitals'
+
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root'),
+)
+
+reportWebVitals(console.debug)
